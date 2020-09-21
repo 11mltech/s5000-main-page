b27be22fa143064fd5ef3b0278c6f57e4e35d72d
Daniel Mantovani <daniel@gmail.com>
2020-09-21 18:11:13 -0300

    sidebar and dashboard

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@
 import Popper from "popper.js";
 window.jQuery = $;
 window.$ = $;
-
+import { gsap } from "gsap";
+window.gsap = gsap;
 require("bootstrap");
 
 // Remove this demo code, that's only here to show how the .env file works!
